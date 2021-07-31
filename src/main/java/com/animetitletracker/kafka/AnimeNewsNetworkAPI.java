b5baaca82cd8d1944056f7c185495a2253c6ff6d
Igor-Laptop <Igordecc@gmail.com>
2021-07-31 23:08:24 +0400

    Parse xml with jsoup

--- src/main/java/com/animetitletracker/kafka/AnimeNewsNetworkAPI.java
+++ src/main/java/com/animetitletracker/kafka/AnimeNewsNetworkAPI.java
@@ -1,10 +1,17 @@
 package com.animetitletracker.kafka;
 
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import java.io.File;
 
 public class AnimeNewsNetworkAPI {
     public static void main(String[] args) {
@@ -17,6 +24,14 @@ public class AnimeNewsNetworkAPI {
         Invocation.Builder builder = target.request();
 
         Response response = builder.get();
-        System.out.println(response.readEntity(String.class));
+
+        String html = response.readEntity(String.class);
+
+        Document doc = Jsoup.parse(html, "", Parser.xmlParser());
+        for (Element e : doc.select("item")) {
+            System.out.println(e);
+        }
+        // clean up resources
+
     }
 }
