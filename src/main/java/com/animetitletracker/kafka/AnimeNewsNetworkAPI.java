05ef5a3201d46c456716495c3c65434e7d570bfb
Игорь Десятов <DesyatovIV@gkosar.corp>
2021-08-30 17:26:31 +0400

    Anime news network divide to blocks

--- src/main/java/com/animetitletracker/kafka/AnimeNewsNetworkAPI.java
+++ src/main/java/com/animetitletracker/kafka/AnimeNewsNetworkAPI.java
@@ -7,7 +7,7 @@ import org.jsoup.parser.Parser;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import java.util.*;
@@ -16,60 +16,82 @@ import java.util.regex.Pattern;
 
 public class AnimeNewsNetworkAPI {
 
-    public static void main(String[] args) throws InterruptedException {
-        int titlesToGet = 50;
-        int NEW_TITLES = 0;
-        HashMap<String, Integer> titleTypes = new HashMap<>();
-        Element checkpointElement = new Element("a");
-        String startFromDate = "2021-07-30 22:26:57";
-        String lastDate = startFromDate;
-        LinkedHashSet<Element> newTitlesList = new LinkedHashSet<>();
+    public static Response getResponse() {
+        int TITLES_TO_GET = 50; // can't fetch more than 50
+        String ID = "148"; //148 and 155
+        String API_URL = "https://www.animenewsnetwork.com/encyclopedia/reports.xml";
 
-        Client client = ClientBuilder.newBuilder().newClient();
-        WebTarget target = client.target("https://www.animenewsnetwork.com/encyclopedia/reports.xml");
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(API_URL);
         target = target
-                .queryParam("id", "148") //148 and 155
-                .queryParam("nlist", titlesToGet);
-        Invocation.Builder builder = target.request();
+                .queryParam("id", ID)
+                .queryParam("nlist", TITLES_TO_GET);
+        Builder builder = target.request();
+        return builder.get();
+    }
+
+    public static Set<Element> parseTitleNames(Document doc, Element checkpointElement, String lastDate) {
+
+        Set<Element> newTitleNamesBuffer = new LinkedHashSet<>();
+
+        System.out.println(doc.select("type").toString());
+        if (newTitleNamesBuffer.isEmpty() && lastDate.isEmpty() && checkpointElement.toString().isEmpty()) {
+            checkpointElement = doc.select("item").first();
+            lastDate = doc.select("date_added").first().text();
+        }else if(!lastDate.isEmpty() && checkpointElement.toString().isEmpty()){
+            checkpointElement = doc.select(String.format("item:added:contains(%s)", lastDate)).first();
+        }
+        else {
+            for (Element element : doc.select("item")) {
+                boolean isDateTheLast = element.toString().equals(
+                        doc.select(String.format("item:contains(%s)", lastDate)).toString());
+                if (isDateTheLast) { break; } else {
+                    newTitleNamesBuffer.add(element);
+                }
+            }
+        }
+        return newTitleNamesBuffer;
+    }
+
+    public static Map<String, Integer> parseTitleTypes(Set<Element> newTitleNamesBuffer) {
+        Map<String, Integer> titleTypesBuffer = new HashMap<>();
+        for (Element element : newTitleNamesBuffer) {
+            String animeName = element.select("anime").text();
+            Matcher matcher = Pattern
+                    .compile("\\(\\w*\\s*\\w\\)")
+                    .matcher(animeName);
+            String animeType = matcher.find() ? matcher.group() : "";
+            if (!titleTypesBuffer.containsKey(animeType)) {titleTypesBuffer.put(animeType, 0);}
+            titleTypesBuffer.put(animeType, titleTypesBuffer.get(animeType) + 1);
+        }
+        return titleTypesBuffer;
+    }
+
+    public static void display(Set<Element> newTitleNamesBuffer, Map<String, Integer> titleTypesBuffer) {
+        System.out.println("NEW TITLES: " + newTitleNamesBuffer.size() );
+        for (String title : titleTypesBuffer.keySet()) {
+            System.out.println(String.format("%s : %s", title, titleTypesBuffer.get(title)));
+        }
+        System.out.println("----------------");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Element CHECKPOINT_ELEMENT = new Element("a");
+        String LAST_DATE = "2021-07-30 22:26:57";
 
         while (true) {
             try {
-                Response response = builder.get();
+                Response response = AnimeNewsNetworkAPI.getResponse();
                 String html = response.readEntity(String.class);
                 Document doc = Jsoup.parse(html, "", Parser.xmlParser());
-                System.out.println(doc.select("type").toString());
-                if (newTitlesList.isEmpty() && lastDate.isEmpty() && checkpointElement.toString().isEmpty()) {
-                    checkpointElement = doc.select("item").first();
-                    lastDate = doc.select("date_added").first().text();
-                }else if(!lastDate.isEmpty() && checkpointElement.toString().isEmpty()){
-                    checkpointElement = doc.select(String.format("item:added:contains(%s)", lastDate)).first();
-                }
-                else {
-                    for (Element element : doc.select("item")) {
-                        boolean isDateTheLast = element.toString().equals(
-                                doc.select(String.format("item:contains(%s)", lastDate)).toString());
-                        if (isDateTheLast) { break; } else {
-                            newTitlesList.add(element);
-                        }
-                    }
-                }
-                NEW_TITLES = newTitlesList.size() ;
-                for (Element element : newTitlesList) {
-                    String animeName = element.select("anime").text();
-                    Matcher matcher = Pattern
-                            .compile("\\(\\w*\\s*\\w\\)")
-                            .matcher(animeName);
-                    String animeType = matcher.find() ? matcher.group() : "";
-                    if (!titleTypes.containsKey(animeType)) {titleTypes.put(animeType, 0);}
-                    titleTypes.put(animeType, titleTypes.get(animeType) + 1);
-                }
-                System.out.println("NEW TITLES: " + NEW_TITLES );
-                for (String title : titleTypes.keySet()) {
-                    System.out.println(String.format("%s : %s", title, titleTypes.get(title)));
-                }
-                titleTypes = new HashMap<>();
-                newTitlesList = new LinkedHashSet<>();
-                System.out.println("----------------");
+
+                Set<Element>  newTitleNamesBuffer = parseTitleNames(doc, CHECKPOINT_ELEMENT, LAST_DATE);
+                Map<String, Integer> titleTypesBuffer = parseTitleTypes(newTitleNamesBuffer);
+
+                display(newTitleNamesBuffer, titleTypesBuffer);
+
+                titleTypesBuffer.clear();
+                newTitleNamesBuffer.clear();
             } catch (Exception e) {
                 System.out.println(e);
             }
